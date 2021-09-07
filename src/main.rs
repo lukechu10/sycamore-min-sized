0ec0dcdc4ba756c5c57b7624183e079fd79a255a
Luke Chu <lukewchu@outlook.com>
2021-09-07 15:26:10 +0000

    wee_alloc

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use sycamore::prelude::*;
 
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 fn main() {
     sycamore::render(|| template! { "Hello World!" });
 }
